7fdc78ca397f609681288747f9a69477e6faf339
appleEmily <emiri3128@gmail.com>
2019-06-16 23:39:13 +0900

    Initial Commit

---
+++ wordListRe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordListRe
+//
+//  Created by 野崎絵未里 on 2019/06/16.
+//  Copyright © 2019年 野崎絵未里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
